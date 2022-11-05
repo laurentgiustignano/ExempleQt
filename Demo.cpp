aeaafb0b1839fd86a66bc07c15e7b0fdf1686cd7
Laurent Giustignano <laurent.giustignano@free.fr>
2022-11-05 14:37:59 +0100

    Ajout d'une classe Demo vide

---
+++ Demo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Laurent Giustignano on 05/11/2022.
+//
+
+#include "Demo.h"
---
+++ Demo.h
@@ -0,0 +1,12 @@
+//
+// Created by Laurent Giustignano on 05/11/2022.
+//
+
+#ifndef EXEMPLEQT_DEMO_H
+#define EXEMPLEQT_DEMO_H
+
+class Demo
+{
+};
+
+#endif //EXEMPLEQT_DEMO_H
